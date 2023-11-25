0c4f430ad89f6a6f48c491f4037b05b1904224e1
Valay <oza.valay09@gmail.com>
2023-11-25 13:12:21 -0500

    improvements

--- script.js
+++ script.js
@@ -1,19 +1,81 @@
-function calculate() {
-    const amount = document.getElementById('amount').value;
-    const interestRate = document.getElementById('interestRate').value;
-    if (!interestRate) {
-        return
+function calculate(className) {
+    if (className === 'payment') {
+        calculatePrincipal()
+    } else if (className === 'principal') {
+        calculatePayment()
     }
-    const monthlyRate = interestRate / 1200
-    const rateTerm = Math.pow((1 + monthlyRate), 25 * 12)
-    const numerator = rateTerm - 1
-    const denominator = monthlyRate * rateTerm
-    const loanAmt = (amount * numerator) / denominator
-    // Simple loan calculation formula (just for demonstration)
-    // const monthlyPayment = amount * (interestRate / 1200) / (1 - Math.pow(1 + interestRate / 1200, -60)); // Assuming a 5-year loan term (60 months)
-    // const borrowAmount = monthlyPayment * 60; // Total amount you can borrow over 5 years -->
+}
+function calculatePrincipal() {
+    try {
+        const amount = document.getElementById('paymentSlider').value;
+        const interestRate = document.getElementById('interestRate').value;
+        const years = document.getElementById('term').value
+        if (!interestRate) {
+            return
+        }
+        const monthlyRate = interestRate / 1200
+        const rateTerm = Math.pow((1 + monthlyRate), years * 12)
+        const numerator = rateTerm - 1
+        const denominator = monthlyRate * rateTerm
+        const loanAmt = (amount * numerator) / denominator
+        const dollarAmt = toDollars(loanAmt)
+        for (var el of document.getElementsByClassName('principal')) {
+            if (el.tagName === 'INPUT') {
+                el.value = el.type === 'text' ? dollarAmt : loanAmt
+            }
+        }
+        // document.getElementById('borrowAmount').innerText = toDollars(loanAmt);
+    } catch (e) {
+        console.error(e)
+    }
+}
 
-    document.getElementById('borrowAmount').innerText = toDollars(loanAmt);
+function calculatePayment() {
+    try {
+        const principal = document.getElementById('principalSlider').value;
+        const interestRate = document.getElementById('interestRate').value;
+        const rate = interestRate / 1200
+        const years = document.getElementById('term').value
+        const rateTerm = Math.pow((1 + rate), years * 12)
+        const numerator = rate * rateTerm
+        const denominator = rateTerm - 1
+        const payment = (principal * numerator) / denominator
+        const dollarAmt = toDollars(payment)
+        for (var el of document.getElementsByClassName('payment')) {
+            if (el.tagName === 'INPUT') {
+                el.value = el.type === 'text' ? dollarAmt : payment
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function updateValue(className, srcId) {
+    // console.log(`triggered from ${srcId}`)
+    const srcVal = fromDollars(document.getElementById(srcId).value);
+    const dollarVal = toDollars(srcVal)
+    const inputElements = document.getElementsByClassName(className)
+    for (let i in inputElements) {
+        var el = inputElements[i]
+        if (el.tagName === 'INPUT' && el.id !== srcId) {
+            const value = el.classList.contains('text') ? dollarVal : srcVal
+            // console.log(`setting value of ${el.id} to ${value}`)
+            el.value = value
+            // console.log(`now value is ${el.value}`)
+        }
+    }
+    calculate(className)
+}
+
+function numToDollars(id) {
+    const el = document.getElementById(id)
+    el.value = toDollars(parseFloat(el.value))
+}
+
+function dollarsToNum(id) {
+    const el = document.getElementById(id)
+    el.value = parseFloat(fromDollars(el.value))
 }
 
 function updateSliderValue() {
@@ -23,23 +85,14 @@ function updateSliderValue() {
 }
 
 function toDollars(num) {
-    num = num.toString()
-    const splitVal = num.split('.')
-    const left = splitVal[0].split('')
-    var i = left.length - 1
-    var numCt = 0
-    var finalVal = ''
-    while (i >= 0) {
-        finalVal = left[i] + finalVal
-        numCt += 1
-        if (numCt === 3 && i !== 0) {
-            numCt = 0
-            finalVal = ',' + finalVal
-        }
-        i -= 1
-    }
-    if (splitVal.length == 2) {
-        finalVal += '.' + splitVal[1].slice(0, 2)
-    }
-    return '$' + finalVal
+    var converted = parseFloat(num).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'CAD'
+    })
+    // console.log(`converting ${num} to ${converted}`)
+    return converted
+}
+
+function fromDollars(dollarVal) {
+    return dollarVal.replace(/[^0-9.-]+/g, '')
 }
